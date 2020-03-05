5e97ec91aa08813c36149a2ed4ac57442e6ac9bd
Ana Maria Grimm <GRIMMANAM@JOHNDEERE.COM>
2020-03-05 16:13:53 -0600

    api data returned from recipe puppy

--- recipes/src/App.js
+++ recipes/src/App.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+import{ Route, Link, Switch, Redirect} from 'react-router-dom';
 import './App.css';
-import dataJson from './recipe.json';
-import { render } from 'react-dom';
+import axios from "axios";
 
-function App(){
-    console.log(dataJson);
+class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      results:[]
+    }
+  }
+
+  gethotdog() {
+    axios({
+      method: 'GET',
+      url: 'http://www.recipepuppy.com/api/?',
+      headers: {'Accept': 'application/json'}
+    })
+
+    .then(response => {
+      console.log(response.data.results);
+      this.setState({
+        results: response.data.results})
+     })
+    .catch(error => {
+      console.log(error);
+    })
+  }
+  componentDidMount(){
+    this.gethotdog()
+  }
+
+  render() {
     return (
-      <div>
-        <p>"Console log the static data in ./recipe.json"</p>
-      </div>
+        <p>"api data returned from recipe puppy"</p>
     );
   }
+}  
 
 export default App;
