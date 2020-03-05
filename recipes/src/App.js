627016d5de4d1982835d72c44d27f31d1c30635a
Ana Maria Grimm <GRIMMANAM@JOHNDEERE.COM>
2020-03-05 14:55:18 -0600

    successful consolelog data in recipe.json

--- recipes/src/App.js
+++ recipes/src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import dataJson from './recipe.json';
+import { render } from 'react-dom';
 
-function App() {
-  return (
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-  );
-}
+function App(){
+    console.log(dataJson);
+    return (
+      <div>
+        <p>"Console log the static data in ./recipe.json"</p>
+      </div>
+    );
+  }
 
 export default App;
