9eb69a2f8c0c2306205d3d8ec1e3044e8506a813
Ana Maria Grimm <GRIMMANAM@JOHNDEERE.COM>
2020-03-09 01:20:22 -0500

    page links work again; lost data after renames

--- recipes/src/App.js
+++ recipes/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import { Route, Link, Switch, Redirect } from 'react-router-dom';
+import { Route, Link, Switch } from 'react-router-dom';
 import './App.css';
-import text from './text.json';
 import Recipe from './Recipe';
 import Search from './Search';
 import Home from './Home';
--- recipes/src/Recipe.js
+++ recipes/src/Recipe.js
@@ -4,33 +4,17 @@ import "./App.css";
 import text from './text.json';
 
 class Recipe extends Component {
-    
+
     render() {
-        let currentRecipe = text.map(item => {
+        let recipes = text.map(item => {
             return (
-                <div className="recipe" key={item.title}>
-
-                    <p><Link to={"/text/" + item.title}>{item.title}</Link>: {item.title}</p> 
-                    <img src={currentRecipe[0].thumbnail[0].href} width="100" height="100" />
-
+                <div className="recipes" key={item.title}>
+                    <p><Link to={"/text/" + item.title}>{item.title}</Link>: {item.title}</p>
                 </div>
-
-                
             )
-            
-        }) 
-        
-        return <div>{currentRecipe}</div>;
-        // let recipeId = this.props.match.params.index
-        // let recipe = this.props.yum.filter((yum, index) => {
-            // return recipeId === recipe.index
-        // })
-            // return (
-                // <div>
+        })
+        return<div>{ recipes } HELP </div>;
 
-                //     {/* <p>{recipe[0].title}</p> */}
-                //     {/* <img src={recipe[0].thumbnail[0].url} width="100" height="100" /> */}
-                // </div>
-    }         
-}
-    export default Recipe;
+    }
+} 
+export default Recipe;
