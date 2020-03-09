084454eaafd7d110f1bb4ef8b5d84071ed8cd838
Ana Maria Grimm <GRIMMANAM@JOHNDEERE.COM>
2020-03-09 11:58:20 -0500

    no data on recipes.js static data

--- recipes/src/Recipe.js
+++ recipes/src/Recipe.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import "./App.css";
 import "./Home.css";
+import "./App"
 import text from './text.json';
 import { Router } from 'react-router';
 
